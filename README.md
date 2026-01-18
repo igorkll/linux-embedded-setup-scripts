663412460573ed1fec8bf7d0faa8865784e0f030
igorkll <frerther@gmail.com>
2026-01-18 15:49:13 +0300

    backup

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ scripts for configuring embedded linux from a running system can be executed fro
 all scripts must be run from root.
 
 ## scripts
+* change_boot_logo.sh - 
 * system_dump.sh path/to/image.img - creates a dump of the entire disk with the partition table and bootloader. causes all disk writes to be completely blocked during dump creation
---
+++ change_boot_logo.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 /path/to/new/logo.png"
+    exit 1
+fi
+
+LOGO="$1"
+
+if [ ! -f "$LOGO" ]; then
+    echo "File not found: $LOGO"
+    exit 1
+fi
+
+THEME_NAME="mytheme"
+THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
+THEME_SCRIPT_PATH="$THEME_DIR/$THEME_NAME.script"
+
+# CREATE THEME
+mkdir -p "$THEME_DIR"
+cp "$LOGO" "$THEME_DIR/logo.png"
+
+tee "$THEME_DIR/$THEME_NAME.plymouth" > /dev/null <<EOF
+[Plymouth Theme]
+Name=$THEME_NAME
+Description=$THEME_NAME
+ModuleName=script
+
+[script]
+ImageDir=$THEME_DIR
+ScriptFile=$THEME_SCRIPT_PATH
+EOF
+
+tee "$THEME_SCRIPT_PATH" > /dev/null <<'EOT'
+plymouth_set_background_color 0 0 0
+
+image = Image("logo.png")
+
+window_width, window_height = Window.GetSize()
+img_width, img_height = image.GetSize()
+x = (window_width - img_width) / 2
+y = (window_height - img_height) / 2
+
+image.Show(x, y)
+EOT
+
+chmod +x "$THEME_SCRIPT_PATH"
+
+
+cp "$THEME_DIR/$THEME_NAME.plymouth" /usr/share/plymouth/themes/default.plymouth
+update-initramfs -u
